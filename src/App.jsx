cda43f63aa4494957d56e454cc5cee8d58f00ec4
shawonvai25 <abdullahalshawon25@gmail.com>
2025-04-19 07:22:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h2>Do some awesome react job 
+      <br />
+      Cause it's literally have no issues in this point 
+
+    </h2>
+    </>
+  ) 
+}
+
+export default App
